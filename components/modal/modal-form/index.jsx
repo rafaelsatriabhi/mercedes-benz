fbba0a1f2ab1b9142c83410284de188dcb96dd85
rafaelsatriabhi <rafael.satriabhi@gmail.com>
2021-11-02 20:11:23 +0700

    some changes

--- components/modal/modal-form/index.jsx
+++ components/modal/modal-form/index.jsx
@@ -7,9 +7,15 @@ const ModalForm = ({ setShowModal }) => {
   const [isOffered, setIsOffered] = useState(false);
 
   const {
-    register, handleSubmit, watch, formState: { errors },
+    register, handleSubmit, watch, formState: { errors }, setValue,
   } = useForm();
 
+  const checkBoxInputkHandler = () => {
+    setIsOffered(!isOffered);
+    if (isOffered) {
+      setValue('offered_price', '');
+    }
+  };
   const onSubmit = (data) => {
     console.log(data);
   };
@@ -17,23 +23,17 @@ const ModalForm = ({ setShowModal }) => {
   return (
     <div aria-hidden onClick={() => setShowModal(false)} className="fixed z-50 h-full w-full bg-black bg-opacity-50 flex justify-center pt-14">
       <div className="flex flex-col justify-evenly text-black h-full bg-blue-100 max-w-xl p-5">
-        <div className="bg-white h-full py-10" aria-hidden onClick={(e) => e.stopPropagation()}>
+        <div className="overflow-y-auto bg-white h-full py-10" aria-hidden onClick={(e) => e.stopPropagation()}>
           <div className="flex flex-col items-center">
             <div className="text-xxl laptop:text-xxxl">Mercedes-Benz</div>
             <div>-Jakarta-</div>
             <div className="text-center">Enter this form below, and get your special offers!</div>
           </div>
           <form onSubmit={handleSubmit(onSubmit)} className="w-full h-full mt-16 text-black px-2">
-            <input {...register('front-name', { required: true })} className="my-2.5 p-2 w-full" placeholder="Nama" type="text" />
-            <input className="my-2.5 p-2 w-full" placeholder="Nomor Telepon" type="number" />
-            <select {...register('estimation-order-date')} className="my-2.5 p-2 w-full" name="" id="">
-              <option value="">Pilih estimasi waktu pembelian</option>
-              <option value="ASAP">Secepatnya</option>
-              <option value="0 - 1 Month">0 - 1 Bulan</option>
-              <option value="2 - 3 Bulan">2 - 3 Bulan</option>
-              <option value="not decided yet">Belum tahu</option>
-            </select>
-            <select {...register('type')} className="my-2.5 p-2 w-full" name="" id="">
+            <input {...register('frontName', { required: true })} className="my-2.5 p-2 w-full" placeholder="Nama" type="text" />
+            <input {...register('phone_number', { required: true, minLength: 9, maxLength: 13 })} className="my-2.5 p-2 w-full" placeholder="Nomor Telepon" type="number" />
+            <input {...register('city', { required: true })} className="my-2.5 p-2 w-full" placeholder="Kota domisili" type="text" />
+            <select {...register('type', { required: true })} className="my-2.5 p-2 w-full">
               <option value="">Pilih model</option>
               <option value="Mercedes-Benz A-Class">Mercedes-Benz A-Class</option>
               <option value="Mercedes-Benz B-Class">Mercedes-Benz B-Class</option>
@@ -43,16 +43,15 @@ const ModalForm = ({ setShowModal }) => {
               <option value="Mercedes-Benz V-Class">Mercedes-Benz V-Class</option>
               <option value="Other">Lainnya</option>
             </select>
-            <div className="flex">
-              <input onClick={() => setIsOffered(!isOffered)} {...register('gotOffered')} type="checkbox" name="" id="" />
-              <div>Saya sudah mendapat penawaran dari dealer lain</div>
+            <div className="flex items-center my-2">
+              <input className="mr-2" onClick={() => checkBoxInputkHandler()} {...register('gotOffered')} type="checkbox" />
+              <div className="text-xs">Saya sudah mendapat penawaran dari dealer lain</div>
             </div>
             {
               isOffered
               && (
               <>
-                <input {...register('other-dealer')} className="my-2.5 p-2 w-full" placeholder="Nama dealer" type="text" />
-                <input {...register('price-offered')} className="my-2.5 p-2 w-full" placeholder="Harga yang ditawarkan (OTR)" type="number" />
+                <input {...register('offered_price')} className="my-2.5 p-2 w-full" placeholder="Harga yang ditawarkan (OTR)" type="number" />
               </>
               )
             }
